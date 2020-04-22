caf2c5f7d5a3afa1827d32b46ee705e4c3a321c1
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-22 11:24:01 -0400

    List of Links exercise completed

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,19 @@
-
-
 const addresses = [
     "https://google.com",
     "https://bing.com",
     "https://duckduckgo.com"
 ];
 
+const navLinks = document.createElement('ul');
+const navContainer = document.querySelector('.js-link-container')
+
+navContainer.appendChild(navLinks);
+
+addresses.forEach(function (address) {
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
+    link.setAttribute('href', address);
+    link.textContent = address;
+    listItem.appendChild(link);
+    navLinks.appendChild(listItem);
+});
